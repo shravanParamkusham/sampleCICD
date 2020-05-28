7835c68b3d79f8d352ea977e772a0addee1c55f0
shravanCBRE <parakushamshravan.kumar@cbre.com>
2020-05-28 09:59:13 +0530

    added VC

---
+++ sampleCICD/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  sampleCICD
+//
+//  Created by Kumar, Parakusham Shravan @ Hyderabad on 28/05/20.
+//  Copyright © 2020 Kumar, Parakusham Shravan @ Hyderabad. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
