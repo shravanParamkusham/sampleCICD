506c640e1a6147389aada4a8120682f0a721f29e
shravanCBRE <parakushamshravan.kumar@cbre.com>
2020-05-28 09:47:19 +0530

    Initial Commit

---
+++ sampleCICD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sampleCICD
+//
+//  Created by Kumar, Parakusham Shravan @ Hyderabad on 28/05/20.
+//  Copyright © 2020 Kumar, Parakusham Shravan @ Hyderabad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
